ddb72a8ac2c26bb9c638245da8eefa38ab20f13d
Rick Ucker <rick.ucker@gmail.com>
2016-01-15 19:18:38 -0500

    Prefer camelCase over CamelCase.

--- albumart.sh
+++ albumart.sh
@@ -28,16 +28,15 @@ if [[ $# == 0 ]]; then
   exit 1
 fi
 
-WorkingDir=$1
-Date=$(eval date +%Y%m%d)
-LogsDir=$thisScriptDir/logs
-eyed3LogFile=$LogsDir/eyed3-$Date.log
-eyed3ErrLogFile=$LogsDir/eyed3-$Date.err.log
-addcoverLogFile=$LogsDir/addcover-$Date.log
-addcoverErrLogFile=$LogsDir/addcover-$Date.err.log
-AddLogFile=$ScriptsDi
+workingDir=$1
+date=$(eval date +%Y%m%d)
+logsDir=$thisScriptDir/logs
+eyed3LogFile=$logsDir/eyed3-$date.log
+eyed3ErrLogFile=$logsDir/eyed3-$date.err.log
+addcoverLogFile=$logsDir/addcover-$date.log
+addcoverErrLogFile=$logsDir/addcover-$date.err.log
 
 echo "Removing existing cover images..."
-find $WorkingDir -name *.mp3 -exec eyeD3 --remove-images {} \+ > $eyed3LogFile 2> $eyed3ErrLogFile
+find $workingDir -name *.mp3 -exec eyeD3 --remove-images {} \+ > $eyed3LogFile 2> $eyed3ErrLogFile
 echo "Adding new cover images..."
-find $WorkingDir -name *mp3 -exec addcover.sh > $addcoverLogFile 2> $addcoverErrLogFile
+find $workingDir -name *mp3 -exec addcover.sh > $addcoverLogFile 2> $addcoverErrLogFile
